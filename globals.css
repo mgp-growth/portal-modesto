bf11d09cb18e9f188d624d673e5687cf29909080
modestogrowth <vinicius.reis@modestogrowth.com.br>
2026-07-13 20:04:43 -0300

    Delete globals.css

--- globals.css
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-// Escopo: pastas e entregas. O admin entra direto em Clientes.
-export default function AdminHome() {
-  redirect("/admin/clients");
-}
